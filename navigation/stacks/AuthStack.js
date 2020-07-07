a8bac264e87138b39ee39def60000c3f9e71b71b
Ekaterina Serga <ekaterina.a.serga@gmail.com>
2020-07-07 18:47:06 -0400

    connect Registration Screen

--- navigation/stacks/AuthStack.js
+++ navigation/stacks/AuthStack.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import { Login } from "../../screens";
+import { Login, RegistrationScreen } from "../../screens";
 import { TabNav } from "./index";
 
 const Auth = createStackNavigator();
@@ -15,7 +15,7 @@ export default function AuthStack(props) {
         {() => <Login {...props} log={props.log} />}
       </Auth.Screen>
       <Auth.Screen name="TabNav" component={TabNav} />
-      {/* ADD REGISTER SCREEN HERE */}
+      <Auth.Screen name="Registration" component={RegistrationScreen} />
     </Auth.Navigator>
   );
 }
