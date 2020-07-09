e4940ad28845f5a453929fd3d6832c2253abfdf5
Cesar Done <cesardone@live.com>
2020-07-09 13:16:16 -0400

    feat: when a user reopens app after closing, and they were logged in, they will be directed to home page instead of log in. todo: currently app shows log in page for a second then refreshes and takes user to home page. Work on showing a loading screen while it updates the user information

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -3,42 +3,90 @@ import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { TabNav, AuthStack } from './stacks/index';
-// import { config, db } from "../firebaseconfig";
+import { firebase } from '../firebaseconfig';
+import AsyncStorage from '@react-native-community/async-storage';
 
 const Stack = createStackNavigator();
 
 export default class AppNavigator extends React.Component {
   constructor() {
     super();
-    this.state = {
-      isLoggedIn: false,
-    };
+    this.state = { isLoggedIn: false };
+    this.log();
+
     this.log = this.log.bind(this);
+    this.logOut = this.logOut.bind(this);
   }
 
-  log() {
-    this.setState({ isLoggedIn: true });
+  // Does not work right now
+  // async getData() {
+  //   const jsonValue = await AsyncStorage.getItem('userInfo');
+  //   if (jsonValue) {
+  //     console.log('json Value in getData:', jsonValue);
+  //     return await JSON.parse(jsonValue);
+  //   } else return;
+  // }
+
+  async log() {
+    // When there is a change on firebase authentication load user data onto application state
+    firebase.auth().onAuthStateChanged(async (user) => {
+      if (user) {
+        const userInfo = {
+          userId: user.uid,
+          displayName: user.displayName,
+          email: user.email,
+          emailVerified: user.emailVerified,
+          photoURL: user.photoURL,
+          providerData: user.providerData,
+          isLoggedIn: true,
+        };
+
+        // Store User info using Async Storage inside userInfo
+        try {
+          const jsonValue = JSON.stringify(userInfo);
+          await AsyncStorage.setItem('userInfo', jsonValue);
+        } catch (e) {
+          console.log(e);
+        }
+
+        // Set state with user information
+        const jsonValue = await AsyncStorage.getItem('userInfo');
+        if (jsonValue) {
+          this.setState(JSON.parse(jsonValue));
+        }
+      }
+    });
+  }
+
+  logOut() {
+    firebase
+      .auth()
+      .signOut()
+      .then(async () => {
+        await AsyncStorage.removeItem('userInfo');
+        this.setState({ isLoggedIn: false });
+      });
   }
 
   render() {
-    console.log(this.state.isLoggedIn);
     return (
       <NavigationContainer>
         <Stack.Navigator
-          // initialRouteName="Login"
           style={styles.container}
           screenOptions={{ headerShown: false }}
         >
-          {console.log('iam here')}
           {this.state.isLoggedIn ? (
             <Stack.Screen
               name='TabNav'
-              component={TabNav}
               screenOptions={{ headerShown: false }}
               options={({ route }) => ({
                 headerTitle: getHeaderTitle(route),
               })}
-            />
+            >
+              {(props) => (
+                <TabNav {...props} userInfo={this.state} logOut={this.logOut} />
+              )}
+            </Stack.Screen>
           ) : (
             <Stack.Screen name='Login'>
               {(props) => <AuthStack {...props} log={this.log} />}
