e49ed17b565461f47a5abf11e3e04d30afaeb8b1
cesardone <cesardone@live.com>
2020-07-10 11:10:58 -0400

    Add files via upload

---
+++ firebaseconfig.js
@@ -0,0 +1,21 @@
+import * as firebase from "firebase";
+import "@firebase/auth";
+import "@firebase/firestore";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyC64IsgzPIll9rolOYBtqpkjLlZ8MMsB3g",
+  authDomain: "cookstarswdf-94cf8.firebaseapp.com",
+  databaseURL: "https://cookstarswdf-94cf8.firebaseio.com",
+  projectId: "cookstarswdf-94cf8",
+  storageBucket: "cookstarswdf-94cf8.appspot.com",
+  messagingSenderId: "409318579317",
+  appId: "1:409318579317:web:e23d98661c6ed70a49ec3c",
+  measurementId: "G-1GYL1VR4VH",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+export { firebase, db };
