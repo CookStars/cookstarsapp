0fb4095e99d7e3a855b6e501f356255615034f9d
asim samuel <asim.samuel1@gmail.com>
2020-07-15 10:54:03 -0400

    Feature: created leaderboard redux store

--- redux/store.js
+++ redux/store.js
@@ -2,6 +2,7 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import recipeReducer from './recipeReducer';
 import userReducer from './userReducer';
 import thunkMiddleware from 'redux-thunk';
+import leaderboardReducer from './leaderboardReducer';
 
 //
 // Store...
@@ -9,6 +10,7 @@ import thunkMiddleware from 'redux-thunk';
 const rootReducer = combineReducers({
   recipes: recipeReducer,
   user: userReducer,
+  users: leaderboardReducer
 });
 
 export default createStore(rootReducer, applyMiddleware(thunkMiddleware));
