a157cab9c9b111de2e94b86156c9d0baf633e5ce
Erica Liu <eliu925@gmail.com>
2020-07-15 17:08:38 -0400

    fix: fixed styling of success page

--- screens/Success.js
+++ screens/Success.js
@@ -1,129 +1,148 @@
-import React from "react";
+import React from 'react'
 import {
-  StyleSheet,
-  Text,
-  ScrollView,
-  View,
-  Button,
-  Image,
-  Alert,
-  SafeAreaView,
-  TouchableOpacity,
-  Dimensions,
-  ImageBackground,
-} from "react-native";
+    StyleSheet,
+    Text,
+    ScrollView,
+    View,
+    Button,
+    Image,
+    Alert,
+    SafeAreaView,
+    TouchableOpacity,
+    Dimensions,
+    ImageBackground,
+} from 'react-native'
 
 //may want to write congrats! you earned __ badge. or __ points to next badge!
 
 export default function SuccessPage(props) {
-  const { navigation } = props;
-  const { index, recipes, userInfo } = props.route.params;
-  const img = recipes[index + 1].image;
-  return (
-    <View style={styles.container}>
-      <View style={styles.image}>
-        <ImageBackground
-          source={require("../assets/cuteavocado.png")}
-          style={styles.avocadoImg}
-        >
-          <Image
-            source={require("../assets/banner.png")}
-            style={styles.banner}
-          />
-        </ImageBackground>
-      </View>
+    const { navigation } = props
+    const { index, recipes, userInfo } = props.route.params
+    const img = recipes[index + 1].image
+    return (
+        <View style={styles.container}>
+            <View style={styles.image}>
+                <Image
+                    source={require('../assets/banner.png')}
+                    style={styles.banner}
+                />
+                <Image
+                    source={require('../assets/avocadodance.jpg')}
+                    style={styles.avocadoImg}
+                />
+            </View>
 
-      <View style={styles.congratsContainer}>
-        <View>
-          <Text style={styles.congratsText}> YAY YOU DID IT </Text>
+            <View style={styles.congratsContainer}></View>
+            <View style={styles.pointsContainer}>
+                <View>
+                    <Image
+                        source={require('../assets/congratsBadge.png')}
+                        style={styles.badge}
+                    />
+                </View>
+                <View style={styles.textContainer}>
+                    <Text style={styles.pointsText}>YOU EARNED 10 PTS!</Text>
+                    <Text style={styles.pointsText}>
+                        TOTAL POINTS: {userInfo.points}
+                    </Text>
+                </View>
+            </View>
+            <View style={styles.tmrwRecipe}>
+                <TouchableOpacity
+                    activeOpacity={0.7}
+                    onPress={() =>
+                        navigation.navigate('SingleRecipe', {
+                            index: index + 1,
+                            recipes: recipes,
+                        })
+                    }
+                >
+                    <Image
+                        source={{ uri: img }}
+                        style={{
+                            width: Dimensions.get('screen').width,
+                            height: 0.33 * Dimensions.get('screen').height,
+                        }}
+                    />
+                    <View style={styles.labelContainer}>
+                        <Text style={styles.tmrwRecipelabel}>
+                            Check Out Tomorrow's Recipe
+                        </Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
         </View>
-      </View>
-      <View style={styles.pointsContainer}>
-        <View>
-          <Image
-            source={require("../assets/congratsBadge.png")}
-            style={styles.badge}
-          />
-          <Text>Point Status {userInfo.points}</Text>
-        </View>
-      </View>
-
-      <View style={styles.tmrwRecipe}>
-        <TouchableOpacity
-          activeOpacity={0.7}
-          onPress={() =>
-            navigation.navigate("SingleRecipe", {
-              index: index + 1,
-              recipes: recipes,
-            })
-          }
-        >
-          <Image
-            source={{ uri: img }}
-            style={{ width: Dimensions.get("screen").width, height: 300 }}
-          />
-          <View style={styles.labelContainer}>
-            <Text style={styles.tmrwRecipelabel}>Tomorrow's Recipe</Text>
-          </View>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // justifyContent: 'space-between',
-    alignItems: "center",
-    flexDirection: "column",
-    alignContent: "space-around",
-    width: "100%",
-  },
-  image: {},
-  banner: {
-    width: Dimensions.get("screen").width,
-    height: 100,
-  },
-  avocadoImg: {
-    width: Dimensions.get("screen").width,
-  },
-  congratsContainer: {
-    flex: 3,
-  },
-  congratsText: {
-    fontSize: 30,
-    alignSelf: "center",
-  },
-  pointsContainer: {
-    flex: 4,
-  },
-  tmrwRecipe: {
-    justifyContent: "flex-end",
-  },
-  badge: {
-    width: 200,
-    height: 200,
-  },
+    container: {
+        backgroundColor: 'white',
+        alignItems: 'center',
+        flexDirection: 'column',
+        alignContent: 'space-around',
+    },
+    image: {
+        width: Dimensions.get('screen').width,
+        height: 0.3 * Dimensions.get('screen').height,
+        paddingTop: '10%',
+        alignItems: 'center',
+    },
+    banner: {
+        width: Dimensions.get('screen').width,
+        height: '50%',
+        resizeMode: 'cover',
+    },
+    avocadoImg: {
+        width: '100%',
+        height: '60%',
+        resizeMode: 'contain',
+        paddingBottom: '10%',
+    },
+ pointsContainer: {
+  borderWidth: 2,
+  borderColor:'grey',
+     flexDirection: 'row',
+     alignContent:'center',
+        width: .95*Dimensions.get('screen').width,
+        justifyContent: 'space-evenly',
+    },
+    textContainer: {
+        justifyContent: 'center',
+    },
+    pointsText: {
+        alignSelf: 'center',
+        fontSize: 18,
+        paddingVertical: 10,
+        color: 'grey',
+        fontWeight: 'bold',
+    },
+    tmrwRecipe: {
+        justifyContent: 'flex-end',
+    },
+    badge: {
+        width: .35*Dimensions.get('screen').width,
+        height: .45*Dimensions.get('screen').width,
+    },
 
-  labelContainer: {
-    position: "absolute",
-    top: "10%",
-    right: "-3%",
-    height: 37,
-    width: 220,
-    borderRadius: 15,
-    backgroundColor: "#E8EDED",
-  },
-  tmrwRecipelabel: {
-    opacity: 0.9,
-    backgroundColor: "transparent",
-    color: "#E07A5F",
-    fontSize: 19,
-    marginTop: 7,
-    marginLeft: 31,
-  },
-});
+    labelContainer: {
+        position: 'absolute',
+        bottom: '3%',
+        height: 40,
+        width: 0.75 * Dimensions.get('screen').width,
+        borderRadius: 15,
+        backgroundColor: '#EF233C',
+        alignSelf: 'center',
+    },
+    tmrwRecipelabel: {
+        opacity: 0.9,
+        backgroundColor: 'transparent',
+        color: 'white',
+        alignSelf: 'center',
+        fontSize: 18,
+        marginTop: 7,
+    },
+})
 
 // const styles = StyleSheet.create({
 //   container: {
