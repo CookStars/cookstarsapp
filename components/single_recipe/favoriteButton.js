515a4ca9781bc38d70e7e2713308abfb35540899
Erica Liu <eliu925@gmail.com>
2020-07-19 16:01:46 -0400

    refactor: refactored SingleRecipe screen

---
+++ components/single_recipe/favoriteButton.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react'
+import HTML from 'react-native-render-html'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import { db } from '../../firebaseconfig'
+import { AntDesign } from '@expo/vector-icons'
+
+export default function favoriteButton(props) {
+    const { userInfo, recipe } = props
+    return (
+        <TouchableOpacity
+            onPress={() => {
+                db.collection('users')
+                    .doc(userInfo.userId)
+                    .update({
+                        favoriteRecipes: {
+                            ...userInfo.favoriteRecipes,
+                            [recipe.id]: recipe,
+                        },
+                    })
+            }}
+            style={styles.favoriteButton}
+        >
+            <View>
+                {userInfo.favoriteRecipes[recipe.id] ? (
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <AntDesign name="heart" size={20} color="white" />
+                        <Text
+                            style={{
+                                color: 'white',
+                                left: 6,
+                            }}
+                        >
+                            FAVORITED
+                        </Text>
+                    </View>
+                ) : (
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <AntDesign name="hearto" size={20} color="white" />
+                        <Text
+                            style={{
+                                color: 'white',
+                                left: 6,
+                            }}
+                        >
+                            FAVORITE
+                        </Text>
+                    </View>
+                )}
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    favoriteButton: {
+        backgroundColor: '#EF233C',
+        position: 'absolute',
+        top: '85%',
+        right: -8,
+        borderRadius: 10,
+        width: '40%',
+        height: 35,
+        alignItems: 'center',
+        alignSelf: 'center',
+        justifyContent: 'center',
+    },
+})
