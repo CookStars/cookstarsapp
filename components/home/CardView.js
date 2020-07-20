7f6478f33f508adf3af01af3a9453b65d301cd20
Ekaterina Serga <ekaterina.a.serga@gmail.com>
2020-07-19 23:44:58 -0400

    refactor home folder colors in components

--- components/home/CardView.js
+++ components/home/CardView.js
@@ -1,89 +1,85 @@
-import React from 'react';
+import React from 'react'
 import {
-  StyleSheet,
-  View,
-  Image,
-  Text,
-  TouchableOpacity,
-  Modal,
-  Dimensions,
-} from 'react-native';
-
-const weekdays = [
-  'Sunday',
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday',
-];
+    StyleSheet,
+    View,
+    Image,
+    Text,
+    TouchableOpacity,
+    Modal,
+    Dimensions,
+} from 'react-native'
+import { colors, weekdays } from '../../utils/constants'
 
 export default function CardView(props) {
-  const { navigation, recipes, userInfo, day } = props;
-  const today = new Date().getDay();
+    const { navigation, recipes, userInfo, day } = props
+    const today = new Date().getDay()
 
-  return (
-    <View style={styles.cardContainer}>
-      {recipes[today] ? (
-        <TouchableOpacity
-          activeOpacity={0.7}
-          onPress={() =>
-            navigation.navigate('SingleRecipe', {
-              day: day,
-              recipe: recipes[today],
-              userInfo: userInfo,
-            })
-          }
-        >
-          <View style={styles.imgContainer}>
-            <Image
-              source={{
-                uri: recipes[today].image
-                  ? recipes[today].image
-                  : 'https://webknox.com/recipeImages/641671-556x370.jpg',
-              }}
-              style={{ width: Dimensions.get('screen').width, height: 300 }}
-            />
-            <View style={styles.labelContainer}>
-              <Text style={styles.recipeOfTheDay}>Recipe of the Day</Text>
-            </View>
-          </View>
-        </TouchableOpacity>
-      ) : (
-        <TouchableOpacity activeOpacity={0.7}>
-          <View style={styles.imgContainer}></View>
-        </TouchableOpacity>
-      )}
-    </View>
-  );
+    return (
+        <View style={styles.cardContainer}>
+            {recipes[today] ? (
+                <TouchableOpacity
+                    activeOpacity={0.7}
+                    onPress={() =>
+                        navigation.navigate('SingleRecipe', {
+                            day: day,
+                            recipe: recipes[today],
+                            userInfo: userInfo,
+                        })
+                    }
+                >
+                    <View style={styles.imgContainer}>
+                        <Image
+                            source={{
+                                uri: recipes[today].image
+                                    ? recipes[today].image
+                                    : 'https://webknox.com/recipeImages/641671-556x370.jpg',
+                            }}
+                            style={{
+                                width: Dimensions.get('screen').width,
+                                height: 300,
+                            }}
+                        />
+                        <View style={styles.labelContainer}>
+                            <Text style={styles.recipeOfTheDay}>
+                                Recipe of the Day
+                            </Text>
+                        </View>
+                    </View>
+                </TouchableOpacity>
+            ) : (
+                <TouchableOpacity activeOpacity={0.7}>
+                    <View style={styles.imgContainer}></View>
+                </TouchableOpacity>
+            )}
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  cardContainer: {
-    borderRadius: 5,
-    width: Dimensions.get('window').width,
-    height: 300,
-  },
-  imgContainer: {
-    width: 411,
-    height: 205,
-  },
-  labelContainer: {
-    position: 'absolute',
-    top: 50,
-    left: -20,
-    height: 37,
-    width: 190,
-    borderRadius: 15,
-    backgroundColor: 'rgba(255,255,255,1)',
-  },
-  recipeOfTheDay: {
-    opacity: 0.9,
-    backgroundColor: 'transparent',
-    color: 'rgba(35,40,58,1)',
-    fontSize: 19,
-    marginTop: 7,
-    marginLeft: 31,
-  },
-});
+    cardContainer: {
+        borderRadius: 5,
+        width: Dimensions.get('window').width,
+        height: 300,
+    },
+    imgContainer: {
+        width: 411,
+        height: 205,
+    },
+    labelContainer: {
+        position: 'absolute',
+        top: 50,
+        left: -20,
+        height: 40,
+        width: 190,
+        borderRadius: 20,
+        backgroundColor: colors.offWhite,
+    },
+    recipeOfTheDay: {
+        opacity: 0.9,
+        backgroundColor: 'transparent',
+        color: colors.text,
+        fontSize: 19,
+        marginTop: 7,
+        marginLeft: 31,
+    },
+})
--- components/home/Cards.js
+++ components/home/Cards.js
@@ -8,6 +8,7 @@ import {
     Modal,
     Dimensions,
 } from 'react-native'
+import { colors } from '../../utils/constants'
 
 export default function Cards(props) {
     const { day, navigation, recipe, userInfo } = props
@@ -80,19 +81,19 @@ const styles = StyleSheet.create({
     },
     labelContainer: {
         position: 'absolute',
-        top: 150,
+        top: 145,
         height: 37,
-        width: 100,
-        borderRadius: 15,
+        width: '60%',
+        borderRadius: 20,
         alignSelf: 'center',
-        backgroundColor: 'rgba(255,255,255,1)',
+        backgroundColor: colors.offWhite,
     },
     recipeOfTheDay: {
         opacity: 0.9,
         backgroundColor: 'transparent',
-        color: 'rgba(35,40,58,1)',
+        color: colors.text,
         fontSize: 19,
-        marginTop: 7,
+        margin: 5,
         textAlign: 'center',
     },
 })
